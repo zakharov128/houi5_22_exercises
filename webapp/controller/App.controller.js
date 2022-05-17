a53495f83f6ef703555d0925fcb44a6ec92c7cc6
ho01 clouddnagmbhgmail.onmicrosoft.com <ho01@clouddnagmbhgmail.onmicrosoft.com>
2022-05-17 08:54:05 +0000

    Übung 5

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,22 +1,70 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageBox"
+    "sap/m/MessageBox",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageBox) {
+    function (Controller, MessageBox, JSONModel, Fragment) {
         "use strict";
 
         return Controller.extend("at.clouddna.training01.zhoui501.controller.App", {
+            
+            _fragmentList: {},
+            
             onInit: function () {
+                let oEditModel = new JSONModel({
+                    editmode: false
+                });
+            
+                this.getView().setModel(oEditModel, "editModel");
 
+                this._showCustomerFragment("DisplayCustomer");
             },
 
-            onSavePressed: function () {
+            _showCustomerFragment: function(sFragmentName){
+                let oPage = this.getView().byId("page");
+            
+                oPage.removeAllContent();
+            
+                if(this._fragmentList[sFragmentName]){
+                    oPage.insertContent(this._fragmentList[sFragmentName]);
+                }else{
+                    Fragment.load({
+                        id: this.getView().createId(sFragmentName),
+                        name: "at.clouddna.training01.zhoui501.view.fragment." + sFragmentName,
+                        controller: this
+                    }).then(function(oFragment){
+                        this._fragmentList[sFragmentName] = oFragment;
+                        oPage.insertContent(this._fragmentList[sFragmentName]);
+                    }.bind(this));
+                }
+            },
+
+            onEditPressed: function(){
+                this._toggleEdit(true);
+            },
+            
+            _toggleEdit: function(bEditMode){
+                let oEditModel = this.getView().getModel("editModel");
+            
+                oEditModel.setProperty("/editmode", bEditMode);
+            
+                this._showCustomerFragment(bEditMode ? "ChangeCustomer" : "DisplayCustomer");
+            },
+            
+            onSavePressed: function(){
                 let oModel = this.getView().getModel();
                 let oData = oModel.getData();
                 MessageBox.success(JSON.stringify(oData));
+            
+                this._toggleEdit(false);
+            },
+            
+            onCancelPressed: function(){
+                this._toggleEdit(false);
             },
 
             genderFormatter: function(sKey){
